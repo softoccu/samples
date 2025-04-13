cb48b85021a6b393598df447548b520877314e9f
softoccu <softoccu@gmail.com>
2025-04-12 18:18:33 -0700

    Add LoRA finetune for flux1 dev

---
+++ finetune/flux.1dev/caland/train.sh
@@ -0,0 +1,34 @@
+accelerate launch \
+  --mixed_precision bf16 \
+  --num_cpu_threads_per_process 1 \
+  sd-scripts/flux_train_network.py \
+  --pretrained_model_name_or_path "/home/keke/fluxgym/models/unet/flux1-dev.sft" \
+  --clip_l "/home/keke/fluxgym/models/clip/clip_l.safetensors" \
+  --t5xxl "/home/keke/fluxgym/models/clip/t5xxl_fp16.safetensors" \
+  --ae "/home/keke/fluxgym/models/vae/ae.sft" \
+  --cache_latents_to_disk \
+  --save_model_as safetensors \
+  --sdpa --persistent_data_loader_workers \
+  --max_data_loader_n_workers 2 \
+  --seed 42 \
+  --gradient_checkpointing \
+  --mixed_precision bf16 \
+  --save_precision bf16 \
+  --network_module networks.lora_flux \
+  --network_dim 4 \
+  --optimizer_type adamw8bit \
+  --learning_rate 8e-4 \
+  --cache_text_encoder_outputs \
+  --cache_text_encoder_outputs_to_disk \
+  --fp8_base \
+  --highvram \
+  --max_train_epochs 16 \
+  --save_every_n_epochs 4 \
+  --dataset_config "/home/keke/fluxgym/outputs/caland/dataset.toml" \
+  --output_dir "/home/keke/fluxgym/outputs/caland" \
+  --output_name caland \
+  --timestep_sampling shift \
+  --discrete_flow_shift 3.1582 \
+  --model_prediction_type raw \
+  --guidance_scale 1 \
+  --loss_type l2 \
